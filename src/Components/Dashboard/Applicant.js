319bee4de1564675f858fe47f18bec94e522b1f6
Debadyuti Mukherjee <debadyuti1010@gmail.com>
2024-06-20 13:56:22 +0530

    minor commit

--- src/Components/Dashboard/Applicant.js
+++ src/Components/Dashboard/Applicant.js
@@ -80,7 +80,6 @@ const Applicant = () => {
             'Object ID': taskApplicant._id,
             'Drive Link': taskApplicant.driveLink,
         }));
-
         const worksheet = XLSX.utils.json_to_sheet(worksheetData);
         const workbook = XLSX.utils.book_new();
         XLSX.utils.book_append_sheet(workbook, worksheet, 'Task Applications');
@@ -88,7 +87,7 @@ const Applicant = () => {
         const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
         const blob = new Blob([excelBuffer], { type: 'application/octet-stream' });
 
-        FileSaver.saveAs(blob, 'task_applications.xlsx');
+        FileSaver.saveAs(blob, `${searchApplications}.xlsx`);
     };
 
     return (
